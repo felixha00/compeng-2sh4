ff4b4e2fbd713928c74676afa30fa545e6a1ca39
Felix <51464337+ashotofmilk@users.noreply.github.com>
2019-09-28 00:41:06 -0400

    update!!

---
+++ Labs/Lab 02/LAB2Q1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void print_vector(double array[], int size)
+{
+  for (int i=0; i<size;i++)
+  {
+    printf("Component %d: %f\n",i+1,array[i]);
+  }
+}
+
+void add_vectors(double vector1[],double vector2[],double vector3[], int n)
+{
+  for (int i = 0; i < n; i++){
+    vector3[i] = vector1[i]+vector2[i];
+    printf ("%f\n",vector3[i]);
+  }
+
+}
+
+double scalar_prod(double vector1[], double vector2[], int n){
+  double scal_prod=0;
+   for (int i = 0; i < n; i++){
+     scal_prod += vector1[i]*vector2[i];
+   }
+   return scal_prod;
+}
+
+double norm2 (double vector1[], double vector2[], int n){
+  
+  double l2norm = 0;
+
+  l2norm = sqrt(scalar_prod(vector1, vector2 , n));
+
+  return l2norm;
+}
+
+int main(void){
+
+  double vectorx[] = {2,4,6};
+  double vectory[] = {0,1,2};
+  double vectorsum[] = {0,0,0};
+  double c; 
+
+  print_vector(vectorx, 3);
+  add_vectors(vectorx, vectory, vectorsum, 3);
+
+  
+  printf("%f",scalar_prod(vectorx, vectory, 3));
+
+  return 0;
+
+}
