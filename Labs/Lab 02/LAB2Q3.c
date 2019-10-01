e639857aa5a0b8559d0eabfcab253ea9f3befd98
Felix <51464337+ashotofmilk@users.noreply.github.com>
2019-10-01 17:02:09 -0400

    update

--- Labs/Lab 02/LAB2Q3.c
+++ Labs/Lab 02/LAB2Q3.c
@@ -3,24 +3,29 @@
 
 void print_diag(int mat[N][N]){
 
-    for (int row = 0; row<N; row++) {
-        for (int col = 0; col<N; col++) {
+    for (int i = 0; i<N; i++) {
+        int row, col; // initializes the row and col
+        col = 0; // sets the column to 0
+        row = i; // set the row to i
+        while (row >= 0){ // array reads vvvvvvvv
             printf("%d ", mat[row][col]);
+            row--; // pulls the row back up
+            col++; // shifts the col to right
         }
-        printf("\n");
+
     }
 
-
-    for (int row = 0; row<N; row++){
-        for (int col = 0; col<N; col++){
-
-            printf("%d ", mat[row][col]);
-            if (col == row ){
-                break;
+    for (int i=1; i<N; i++){
+        int row2,col2;
+        row2 = N-1;  //
+        col2 = i; // column increases instead now
+        while (col2 < N) { // array reads >>>>>>
+            printf ("%d ", mat[row2][col2]);
+            row2--; // pulls row back up
+            col2++; // shifts col to right
             }
         }
     }
-}
 
 int main(){
     int mat[N][N] = {{1,12,13,49},{5,16,17,81},{9,10,11,20},{2,45,19,14}};
