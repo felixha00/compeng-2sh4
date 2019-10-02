454cf84065481d5ff7e249095e3177717e7a4eb0
Felix <51464337+ashotofmilk@users.noreply.github.com>
2019-10-02 19:30:05 -0400

    u

--- Labs/Lab 02/LAB2Q7.c
+++ Labs/Lab 02/LAB2Q7.c
@@ -3,9 +3,20 @@
 void addEff(int val1[], int val2[], int val3[],int pos1[], int pos2[],int pos3[],int k1, int k2)
 {
 
+    for (int i = 0; i < k1; i++){ // length of vector 1
+
+    }
+
+    // reuse the reconstruct from part 6
 }
 
 int main()
 {
+    int val1[] = {2, 4, 6};
+    int pos1[] = {2, 4, 6};
+    int val2[] = {1, 3, 5};
+    int pos2[] = {1, 3, 5,};
+    int k2 = 3;
+    int k1 = 3;
 
 }
