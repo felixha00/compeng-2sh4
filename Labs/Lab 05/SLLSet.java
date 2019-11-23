eee6cbf6ae1be57faa3e8a0c917e5db5f2367235
Felix <51464337+ashotofmilk@users.noreply.github.com>
2019-11-23 02:46:18 -0500

    update

--- Labs/Lab 05/SLLSet.java
+++ Labs/Lab 05/SLLSet.java
@@ -1,5 +1,4 @@
 
-
 public class SLLSet {
     private int numElem;
     private SLLNode fNode;
@@ -7,11 +6,13 @@ public class SLLSet {
     public SLLSet(){ numElem = 0; fNode = null;}
 
     public SLLSet(int[] sortedArray){
+
         if (sortedArray == null){ // checks to make sure that the array isnt empty
             this.numElem = 0; 
             this.fNode = null;
             return; 
         }
+
         this.numElem = sortedArray.length;
         this.fNode = new SLLNode(sortedArray[0], null); // makes the first node in the list
         SLLNode currentNode = this.fNode; // sets the current node to be the first one 
@@ -22,14 +23,16 @@ public class SLLSet {
         }   
     }
 
-    public int getSize() {return this.numElem;}
+    public int getSize() {
+        return this.numElem;
+    }
     
     public SLLSet copy(){
         
         SLLSet setcpy = new SLLSet();
         setcpy.numElem = this.numElem;
 
-        SLLNode currentNode = this.fNode; // sets the currentnode to be the firstnode
+        SLLNode currentNode = this.fNode; // sets the currentnode to be the fNode
         SLLNode cpyNode =  new SLLNode(currentNode.value, null);
         setcpy.fNode = cpyNode;
         
@@ -46,44 +49,62 @@ public class SLLSet {
 
     public boolean isIn(int v){
         SLLNode currentNode = this.fNode;
-        while (currentNode.next != null){
+        while (currentNode != null){
             if (v == currentNode.value){
                 return true;
             }
             currentNode = currentNode.next;
         }
+
+    
         return false;
     }
 
     public void add(int v){
+
+        if (this.numElem==0) { //if the list is empty, create the first node
+			SLLNode nNode = new SLLNode(v,null);
+			this.numElem++;
+			this.fNode = nNode;
+			return;
+        }
+        
         SLLNode current = this.fNode;
 
         if (this.isIn(v) == true){
-            System.out.println("Value already exists");
             return;
         }
-        /*
-        if (v < this.fNode.value){ // check if the value is smaller than the firstNode 
+
+        else {
+        
+        if (v < current.value){ // check if the value is smaller than the fNode 
             this.fNode = new SLLNode(v, this.fNode);
             this.numElem++;
             return; 
         }
-        while (current != null){
-            if (v >= current.value){
+
+        while (current.next != null){
+            if (v > current.value && v < current.next.value){ // check that the value v is greater than the previous and less than the next
                 current.next = new SLLNode(v, current.next);
                 this.numElem++;
                 return;
-            }
+            }            
+            current = current.next;
         }
-        */
+        
+        // very last node. doesn't need a condition because all cases will already be covered.
+        
+        current.next = new SLLNode(v,null);
+		this.numElem++;
         return;
+        
+        }
     }
 
     public void remove(int v){
 
         SLLNode current = fNode; 
         if (this.isIn(v) == false){ // check if value doesnt exist
-            System.out.println("Value doesn't exist");
             return;
         }
 
@@ -92,25 +113,96 @@ public class SLLSet {
             this.numElem--;
             return; 
         }
-        while (this.fNode != null){
 
+        while (current.next != null){
+            if (v == current.next.value){
+                current.next = new SLLNode (v, ) 
+            }
         }
+
     }
     public SLLSet union(SLLSet s){
-        SLLSet unionSet = copy(this);
+
+        if (this.fNode == null || s.fNode == null) {
+			SLLSet union = new SLLSet();
+			return union;
+        }
+        
+		SLLSet union = s.copy();
+
+		SLLNode s1 = this.fNode;
+		SLLNode s2 = s.fNode;
+		
+		for (int i=0;i<this.numElem;i++) {
+			union.add(s1.value);
+			s1 = s1.next;
+		}
+		
+		return union;
     }
 
     public SLLSet intersection(SLLSet s){
-        return s;
+        SLLSet intersect = new SLLSet();
+        
+        if (this.fNode == null || s.fNode == null) 
+        {
+            return intersect;
+        }
+
+		SLLNode current = this.fNode;
+		for(int i=0;i<this.numElem;i++) {
+			if(s.isIn(current.value)) {
+				intersect.add(current.value);
+			}
+			current = current.next;
+        } 
+        
+        return intersect;
     }
 
 
     public SLLSet difference(SLLSet s){
-        return s;
+
+        SLLSet diff = new SLLSet();
+        
+        if (this.fNode == null) { // check that fNode is a null to return empty set
+            return diff;
+        }
+        
+        if (s.fNode == null) {
+        diff = this.copy();
+        return diff;
+    }
+
+		SLLNode s1 = this.fNode;
+		SLLNode s2= s.fNode;
+		if (this.numElem>=s.getSize()) {
+			for(int i=0;i<this.numElem;i++) {
+				if(!s.isIn(s1.value)) {
+					diff.add(s1.value);
+				}
+				s1 = s1.next;
+			}
+		} else {
+			for(int i=0;i<s.numElem;i++) {
+				if(!this.isIn(s2.value)) {
+					diff.add(s2.value);
+				}
+				s2 = s2.next;
+			}
+		} 
+		return diff;
     }
 
     public static SLLSet union(SLLSet[] sArray){
-        return null;
+
+            SLLSet unionSet = sArray[0];
+            for (int i=1;i<sArray.length;i++) {
+                unionSet = unionSet.union(sArray[i]);
+            }
+            return unionSet;
+
+        
     }
 
     public String toString(){
--- Labs/Lab 05/ha_felix_400190708_lab05_section03_question1.java
+++ Labs/Lab 05/ha_felix_400190708_lab05_section03_question1.java
@@ -87,6 +87,8 @@ public class ha_felix_400190708_lab05_section03_question1 {
             System.out.println("*************************************************************"); 
             
             //test remove
+
+            /*
             listObj2= new SLLSet(arr);
             listObj2.remove(notremove);
             System.out.println("Test 10 - remove(the value is not in the list, then do nothing):  "+notremove+"\n"+"List:\n"+listObj2.toString());
@@ -109,6 +111,7 @@ public class ha_felix_400190708_lab05_section03_question1 {
             System.out.println("Test 14 - remove(from empty list):  "+removefirst+"\n"+"List:\n"+emp.toString());
             System.out.println("size="+emp.getSize());
             System.out.println("*************************************************************"); 
+            */
             
             // test union
             listObj2= new SLLSet(arr);
